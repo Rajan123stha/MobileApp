b01fa51dfce28a3dec358fc9b4ef552fe129ecf6
Rajan <rajan1234stha@gmail.com>
2024-07-09 20:30:08 +0545

    Moblie with with fragment

---
+++ app/src/main/java/com/example/mobileapp/MenuPage.java
@@ -0,0 +1,21 @@
+package com.example.mobileapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_menu_page);
+
+
+    }
+}
